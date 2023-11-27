9581be2a8bc22e5486ca9cb45a5e4c7170b2463d
rajesh <rajeshb0202@gmail.com>
2023-11-27 16:54:50 +0000

    Discarded the previous rotation method as it was not working properly. Now planning to add rotation method based upon odom position feedback. In this commit, two mutually callbacks grups are created- one for scan and another for odom, so as to provide the data parallely. Need to add the rotation algorithm.

--- attach_shelf/src/pre_approach.cpp
+++ attach_shelf/src/pre_approach.cpp
@@ -1,11 +1,18 @@
 #include "geometry_msgs/msg/detail/twist__struct.hpp"
+#include "nav_msgs/msg/detail/odometry__struct.hpp"
 #include "rcl_interfaces/msg/detail/parameter_descriptor__struct.hpp"
+#include "rclcpp/callback_group.hpp"
+#include "rclcpp/executors/multi_threaded_executor.hpp"
 #include "rclcpp/publisher.hpp"
 #include "rclcpp/rate.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp/subscription_options.hpp"
 #include "rclcpp/utilities.hpp"
 #include <geometry_msgs/msg/twist.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <chrono>
 #include <memory>
 
@@ -18,16 +25,35 @@ class PreApproach : public rclcpp::Node
     public:
         PreApproach():Node("pre_approach_node")
         {
+
+            RCLCPP_INFO(this->get_logger(), "pre_approach node has started!...");
             //defining parameters
             this->declare_parameter("obstacle", 0.0);
             this->declare_parameter("degrees", 0);
 
+            //intialising the variables
             getting_params();
             dest_reached = false;
             rotate_status = false;
 
+            //callback groups
+            odom_callback_group = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+            scan_callback_group = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+
+            //creating subscriptions options
+            rclcpp::SubscriptionOptions odom_options;
+            odom_options.callback_group = odom_callback_group;
+
+            rclcpp::SubscriptionOptions scan_options;
+            scan_options.callback_group = scan_callback_group;
+
+
+            //creating subscribers and publishers
             publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/robot/cmd_vel", 10);
-            subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>("/scan", 10, std::bind(&PreApproach::scan_callback, this, _1));
+            scan_subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>("/scan", 10, std::bind(&PreApproach::scan_callback, this, _1), odom_options);
+            odom_subscription_ = this->create_subscription<nav_msgs::msg::Odometry>("/odom", 10, std::bind(&PreApproach::odom_callback, this, _1), scan_options);
+            
+            RCLCPP_INFO(this->get_logger(), "cmd_vel publisher intialised!...");
         }
 
     private:
@@ -37,14 +63,36 @@ class PreApproach : public rclcpp::Node
             angle_to_be_rotated_parameter = (this->get_parameter("degrees").get_parameter_value().get<int>()) *  (M_PI / 180.0);
         }
 
+
+
+        void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+        {
+            RCLCPP_INFO(this->get_logger(), "inside odom callback method");
+            tf2::Quaternion quat;
+            tf2::fromMsg(msg->pose.pose.orientation, quat);
+
+            double roll, pitch, yaw;
+            tf2::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+
+            RCLCPP_INFO(this->get_logger(), "Yaw: [%f]", yaw);
+
+            if (dest_reached && !rotate_status)
+            {
+                rotate_robot();
+            }
+        }
+
+
+
         void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
         {
-
             front_laser_array_index= int((0 - msg->angle_min)/(msg->angle_increment));
             front_laser_reading = msg->ranges[front_laser_array_index];
 
+    
             if (front_laser_reading > obstacle_distance_parameter  && !dest_reached)
             {
+               RCLCPP_INFO(this->get_logger(), "front_laser_reading: %f", front_laser_reading);
                vel_msg.linear.x = translation_speed;
             }
             else
@@ -54,72 +102,54 @@ class PreApproach : public rclcpp::Node
             }
             
             publisher_->publish(vel_msg);
-
-            if (dest_reached && !rotate_status)
-            {
-                rotate_robot();
-            }
         }
+
+
 
 
         void rotate_robot()
         {
-            float rotation_time = std::abs(angle_to_be_rotated_parameter/angular_speed);
-            
-            //setting the angular speed according to the sign
-            vel_msg.linear.x = 0.0;
-            if(angle_to_be_rotated_parameter > 0)
-            {
-                vel_msg.angular.z = angular_speed;
-            }
-            else if (angle_to_be_rotated_parameter < 0)
-            {
-                vel_msg.angular.z = -1*angular_speed;
-            }
-            else {
-                vel_msg.angular.z = 0;
-                return;
-            }
+            RCLCPP_INFO(this->get_logger(), "inside rotate method");
 
-            RCLCPP_INFO(this->get_logger(), "Rotation started!");
-            rclcpp::Rate loop_rate(1000);
-
-            auto start_time = this->now();
-            while((this->now() - start_time).seconds() < rotation_time)
-            {
-                publisher_->publish(vel_msg);
-                loop_rate.sleep();
-                //std::this_thread::sleep_for(std::chrono::milliseconds(10));
-            }
-
-            vel_msg.angular.z = 0.0;
-            publisher_->publish(vel_msg);
-            RCLCPP_INFO(this->get_logger(), "Rotation executed!");
-            rotate_status = true;
+            //write the rotation algorithm here
         }
 
 
 
     //defining variables
-    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_ ;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
-    geometry_msgs::msg::Twist vel_msg;
+        rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_subscription_ ;
+        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscription_;
+        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+        
+        geometry_msgs::msg::Twist vel_msg;
+        //nav_msgs::msg::Odometry odom_msg;
 
-    int front_laser_array_index;
-    float front_laser_reading;
-    float obstacle_distance_parameter;
-    int angle_to_be_rotated_parameter;
-    bool dest_reached;
-    bool rotate_status;
-    float angular_speed = 0.3;
-    float translation_speed = 0.6;
+        rclcpp::CallbackGroup::SharedPtr odom_callback_group;
+        rclcpp::CallbackGroup::SharedPtr scan_callback_group;
 
+        int front_laser_array_index;
+        float front_laser_reading;
+        float obstacle_distance_parameter;
+        int angle_to_be_rotated_parameter;
+        bool dest_reached;
+        bool rotate_status;
+        float angular_speed = 0.3;
+        float translation_speed = 0.6;
+        float yaw_angle;
 };
+
+
+
 
 int main (int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<PreApproach>());
+    auto pre_approach_node_obj = std::make_shared<PreApproach>();
+
+    rclcpp::executors::MultiThreadedExecutor executor;
+    executor.add_node(pre_approach_node_obj);
+    executor.spin();
+
     rclcpp::shutdown();
     return 0;
 }

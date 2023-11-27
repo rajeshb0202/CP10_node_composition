364b3af494fcbebe4ee6e717c6868755e673fddb
rajesh <rajeshb0202@gmail.com>
2023-11-27 03:41:56 +0000

    Added parameters: obstacle distance and degrees to be rotated to the pre_approach.cpp file. Need to add rotation algorithm.

--- attach_shelf/src/pre_approach.cpp
+++ attach_shelf/src/pre_approach.cpp
@@ -1,4 +1,5 @@
 #include "geometry_msgs/msg/detail/twist__struct.hpp"
+#include "rcl_interfaces/msg/detail/parameter_descriptor__struct.hpp"
 #include "rclcpp/publisher.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/utilities.hpp"
@@ -16,21 +17,34 @@ class PreApproach : public rclcpp::Node
     public:
         PreApproach():Node("pre_approach_node")
         {
-            
+            //defining parameters
+            this->declare_parameter("obstacle", 0.3);
+            this->declare_parameter("degrees", 90.0);
+
+            getting_params();
+
+
             publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/robot/cmd_vel", 10);
             subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>("/scan", 10, std::bind(&PreApproach::scan_callback, this, _1));
         }
 
     private:
+        void getting_params()
+        {
+            obstacle_distance_parameter = this->get_parameter("obstacle").get_parameter_value().get<float>();
+            degree_to_be_rotated_parameter = this->get_parameter("degrees").get_parameter_value().get<float>();
+        }
+
         void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
         {
-            front_laser_array_index= int((msg->angle_max - msg->angle_min)/(2*msg->angle_increment));
-            front_laser_reading = msg->ranges[front_laser_array_index];
-            RCLCPP_INFO(this->get_logger(), "the front laser_reading is: %f", front_laser_reading);
 
-            if (front_laser_reading > distance_gap_threshold)
+            front_laser_array_index= int((0 - msg->angle_min)/(msg->angle_increment));
+            front_laser_reading = msg->ranges[front_laser_array_index];
+            RCLCPP_INFO(this->get_logger(), "front_index: %d, front laser_reading is: %f",front_laser_array_index, front_laser_reading);
+
+            if (front_laser_reading > obstacle_distance_parameter)
             {
-               vel_msg.linear.x = 0.2;
+               vel_msg.linear.x = 0.4;
             }
             else
             {
@@ -50,7 +64,9 @@ class PreApproach : public rclcpp::Node
 
     int front_laser_array_index;
     float front_laser_reading;
-    float distance_gap_threshold = 0.3;
+    float obstacle_distance_parameter;
+    float degree_to_be_rotated_parameter;
+    //float distance_gap_threshold = 0.3;
 
 };
 
